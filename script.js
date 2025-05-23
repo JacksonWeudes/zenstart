bb2789c9fd39c62e8fe17e13b92882b494e59e64
JacksonWeudes <weudes2024jackson@gmail.com>
2025-05-23 16:24:57 -0300

    slider temporario

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ closeBtn.addEventListener("click", () => {
     menu.style.left = "-100%"
     document.body.classList.remove("no-scroll")
 })
+
+
+new Swiper('.testimonials-container', {
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakPoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
